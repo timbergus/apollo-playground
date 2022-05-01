de77c51a707402cd564a7c6cf792f23e4fd1c6eb
Gustavo Muñoz <timbergus@gmail.com>
2022-05-01 21:45:23 +0200

    Add styles.

--- apollo-client/src/app/App.tsx
+++ apollo-client/src/app/App.tsx
@@ -2,56 +2,83 @@ import styled from 'styled-components'
 import { FC, MouseEvent } from 'react'
 
 import { CartProducts } from '../components/Cart/Cart'
-import { UserDetail } from '../components/UserDetail/UserDetail'
 
-import { useUsers } from '../api/useUsers.hook'
-import { useProducts } from '../api/useProducts.hook'
 import { cartProductsVar } from '../client'
+import { useProducts } from '../api/useProducts.hook'
 
-const Product = styled.div`
+const Container = styled.div`
+  display: flex;
+  width: 50vw;
+  flex-direction: column;
+  margin: auto;
+  padding: 2rem 0;
+`
+
+const Shop = styled.div`
+  display: flex;
+  gap: 1rem;
+`
+
+const Products = styled.div`
+  flex: 1;
+  border: 1px solid lightgrey;
+  padding: 1rem;
+  border-radius: 1rem;
+  background-color: #bdbcbc;
+`
+
+type ProductProps = {
+  $color?: string
+}
+
+const Product = styled.div<ProductProps>`
   cursor: pointer;
+  user-select: none;
+  border: 2px solid rgb(${({ $color }): string => $color || 'transparent'});
+  padding: 0.8rem 0.5rem;
+  border-radius: 0.5rem;
+  background-color: rgba(
+    ${({ $color }): string => $color || 'transparent'},
+    0.2
+  );
+
+  & + & {
+    margin-top: 0.5rem;
+  }
 `
 
 export const App: FC = () => {
-  const { loading, error, users } = useUsers()
-  // const { addCartProduct } = useAddCartProduct()
-  const {
-    loading: loadingProducts,
-    error: errorProducts,
-    products,
-  } = useProducts()
+  const { loading, error, products } = useProducts()
 
   const handleAddToCart = (event: MouseEvent) => {
     const { id } = event.currentTarget
     const product = products?.find((product) => product?.id === id)
     if (product) {
-      // addCartProduct(product)
       cartProductsVar([...cartProductsVar(), product])
     }
   }
 
-  if (loading || loadingProducts) return <div>Loading...</div>
+  if (loading) return <div>Loading...</div>
 
-  if (error || errorProducts) return <div>Error!</div>
+  if (error) return <div>Error!</div>
 
   return (
-    <>
-      {users?.map((user) => (
-        <div key={user.id}>
-          User Id: {user.id}
-          <UserDetail id={user.id} />
-        </div>
-      ))}
-      {products?.map((product) => (
-        <Product
-          key={product.id}
-          id={product.id.toString()}
-          onClick={handleAddToCart}
-        >
-          Name: {product.name}
-        </Product>
-      ))}
-      <CartProducts />
-    </>
+    <Container>
+      <Shop>
+        <Products>
+          {products?.map((product) => (
+            <Product
+              key={product.id}
+              id={product.id.toString()}
+              onClick={handleAddToCart}
+              $color={product.color}
+            >
+              {product.name}
+            </Product>
+          ))}
+        </Products>
+        <CartProducts />
+      </Shop>
+    </Container>
   )
 }
--- apollo-client/src/components/Cart/Cart.tsx
+++ apollo-client/src/components/Cart/Cart.tsx
@@ -1,6 +1,48 @@
+import styled from 'styled-components'
 import { FC, useMemo } from 'react'
 
+import { cartProductsVar } from '../../client'
 import { useGetCartProducts } from '../../state/useCartProducts.hook'
+
+const Container = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid lightgrey;
+  padding: 1rem;
+  border-radius: 1rem;
+  gap: 1rem;
+`
+
+const Title = styled.div`
+  font-size: 1.5rem;
+`
+
+const Products = styled.div`
+  flex: 1;
+`
+
+const Total = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 1.5rem;
+`
+
+const Button = styled.button`
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  padding: 8px 32px;
+  border-radius: 0.8rem;
+  box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+
+  &:active {
+    box-shadow: none;
+  }
+`
 
 type CartProductsItem = {
   name: string
@@ -31,18 +73,34 @@ export const CartProducts: FC = () => {
     [cartProducts]
   )
 
+  const total = useMemo(() => {
+    return products.reduce((acc, current) => {
+      return acc + current.quantity
+    }, 0)
+  }, [products])
+
+  const handleClear = () => {
+    cartProductsVar([])
+  }
+
   if (loading) return <div>Loading...</div>
 
   if (error) return <div>Error!</div>
 
   return (
-    <>
-      Cart Products:
-      {products.map((cartProduct) => (
-        <div>
-          {cartProduct.name} - {cartProduct.quantity}
-        </div>
-      ))}
-    </>
+    <Container>
+      <Title>Cart</Title>
+      <Products>
+        {products.map((cartProduct) => (
+          <div>
+            {cartProduct.name} ({cartProduct.quantity})
+          </div>
+        ))}
+      </Products>
+      <Total>
+        <div>Total: {total}</div>
+        <Button onClick={handleClear}>Clear</Button>
+      </Total>
+    </Container>
   )
 }
